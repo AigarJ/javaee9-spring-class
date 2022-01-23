e56a932d22f07525bf4633aa3f3a16a708b2f469
AigarJ <aigar134@gmail.com>
2022-01-23 13:50:28 +0200

    [change] - dynamic html template with name and surname provided based on query parameters

--- src/main/java/com/sda/javaee9spring/controller/FirstController.java
+++ src/main/java/com/sda/javaee9spring/controller/FirstController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,10 +46,14 @@ public class FirstController {
     // surname param with value jurise
     @GetMapping("/my-name")
     public String myName(@RequestParam(value = "name", defaultValue = "John") String myName,
-                         @RequestParam(value = "surname", defaultValue = "McClane") String mySurname){
+                         @RequestParam(value = "surname", defaultValue = "McClane") String mySurname,
+                         Model pageParameters){
         log.info("myName() method was called!!!");
         log.info("my name is: [{}] and my surname is: [{}]", myName, mySurname);
         log.info(String.format("my name is: [%s] and my surname is: [%s]", myName, mySurname));
+
+        pageParameters.addAttribute("myName", myName)
+        .addAttribute("mySurname", mySurname);
 
         return "pages/name-and-surname";
     }
