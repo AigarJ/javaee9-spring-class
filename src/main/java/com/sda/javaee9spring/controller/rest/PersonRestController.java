968ed48e1681fbcae5e7500d2ac8ad3747e91070
AigarJ <aigar134@gmail.com>
2022-02-19 11:22:06 +0200

    [refactor] - changing persons/{id} endpoint so its structure is simpler

--- src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
+++ src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
@@ -5,6 +5,8 @@ import com.sda.javaee9spring.entity.PersonEntity;
 import com.sda.javaee9spring.service.RealPersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,10 +34,18 @@ public class PersonRestController {
         return personService.readAllPersonEntities();
     }
 
+    // /persons/1
+    // /persons/10
+    // /persons/123456
     @GetMapping("/persons/{id}")
-    public PersonEntity findPersonEntityById(@PathVariable("id") Long personId){
+    public ResponseEntity<PersonEntity> findPersonEntityById(@PathVariable("id") Long personId) {
         log.info("trying to find person entity by id: [{}]", personId);
+        var personEntity = personService.readPersonEntityById(personId);
+        if (personEntity == null)
+            return ResponseEntity.notFound().build();
+  //          return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);;
 
-        return personService.readPersonEntityById(personId);
+        return ResponseEntity.ok(personEntity);
+  //      return new ResponseEntity<>(personEntity, null, HttpStatus.OK);
     }
 }
